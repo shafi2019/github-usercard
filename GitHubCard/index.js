b3b154e6c475b9aebd2ffe825f7b28b3520e291b
Shafi Masoumi <masoumi2017@icloud.com>
2019-09-12 15:21:07 -0700

    axios added

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/shafi2019')
+
+.then((response) => {
+  console.log(response);
+})
+.catch((error) => {
+  console.log(error);
+})
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
